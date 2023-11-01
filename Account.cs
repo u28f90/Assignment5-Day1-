a9442235352bbcc848d62e003601e21f1009bf76
u28f90 <vinay.goyal@avl.com>
2023-11-01 08:38:04 +0530

    Add Assignment5

---
+++ Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class Account
+{
+    int id;
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    String accountType;
+    public string AccountType 
+    { get { return accountType; } 
+      set { accountType = value; } 
+    }
+    double balance;
+    public double Balance { 
+        get { return balance; } 
+        set { balance = value; } 
+    }
+
+    public bool Withdraw(double amount)
+    {
+        if (amount < balance)
+        {
+            return true;
+        }
+        else { return false; }
+
+    }
+    public String GetDetails()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append($"Accound Id : {id} \n");
+        sb.Append($"Account Type : {accountType} \n");
+        sb.Append($"Balance : {balance} ");
+        return sb.ToString();
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Account account = new Account();
+        // account.GetDetails();
+        Console.WriteLine("Enter account id");
+        account.Id = Byte.Parse(Console.ReadLine());
+        Console.WriteLine("Enter account type");
+        account.AccountType = Console.ReadLine();
+        Console.WriteLine("Enter account balance");
+        account.Balance = Double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter amount to withdraw");
+        double amountWith = Double.Parse(Console.ReadLine());
+        Console.WriteLine(account.GetDetails());
+        if (account.Withdraw(amountWith))
+        {
+            Console.WriteLine($"New Balance: {account.Balance - amountWith}");
+        }
+
+
+
+    }
+
+}
